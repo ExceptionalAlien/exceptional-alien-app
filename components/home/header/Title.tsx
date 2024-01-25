0f671fda7c0ad1581add83b0afe7e0fa14310a7a
Adam Johnson <adam@adamjohnson.org>
2024-01-25 11:19:04 +1100

    Init

---
+++ components/home/header/Title.tsx
@@ -0,0 +1,58 @@
+import { View, StyleSheet, Text, useColorScheme } from "react-native";
+import { Image } from "expo-image";
+import { Destination } from "../Map";
+import { styleVars } from "utils/styles";
+
+interface TitleProps {
+  destination: Destination;
+}
+
+export default function Title(props: TitleProps) {
+  const colorScheme = useColorScheme();
+  const themeCoordsStyle = colorScheme === "light" ? styles.lightCoords : styles.darkCoords;
+
+  return (
+    <View style={styles.container}>
+      <Image source={require("assets/img/logo-icon.svg")} style={styles.logo} />
+
+      <View>
+        <Text style={styles.destination} allowFontScaling={false}>
+          {props.destination.name}
+        </Text>
+
+        <Text style={[styles.coords, themeCoordsStyle]} allowFontScaling={false}>
+          {Math.abs(props.destination.region.latitude).toFixed(5)}°{props.destination.region.latitude < 0 ? "S" : "N"},
+          {Math.abs(props.destination.region.longitude).toFixed(5)}°{props.destination.region.longitude < 0 ? "W" : "E"}
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    columnGap: 8,
+  },
+  logo: {
+    width: 29,
+    height: 36,
+    tintColor: styleVars.eaBlue,
+  },
+  destination: {
+    fontFamily: "Neue-Haas-Grotesk-Med",
+    fontSize: 24,
+    color: styleVars.eaBlue,
+  },
+  coords: {
+    fontFamily: "Helvetica-Monospaced",
+    fontSize: 12,
+  },
+  lightCoords: {
+    color: styleVars.eaGrey,
+  },
+  darkCoords: {
+    color: "white",
+  },
+});
---
+++ utils/styles.ts
@@ -0,0 +1,6 @@
+export const styleVars = {
+  eaBlue: "rgb(34 32 193)",
+  eaGrey: "rgb(124 124 124)",
+  eaLightGrey: "rgb(197 197 197)",
+  eaRed: "rgb(255 60 0)",
+};
