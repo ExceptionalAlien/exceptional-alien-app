ab8f61f7f4f1c333e563e21941a5fc1a1c96b3e3
Adam Johnson <adam@adamjohnson.org>
2024-03-27 16:43:40 +1100

    Update Title.tsx

--- components/home/header/Title.tsx
+++ components/home/header/Title.tsx
@@ -1,18 +1,28 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Pressable } from "react-native";
 import { Image } from "expo-image";
+import { useRouter } from "expo-router";
 import { DestinationType } from "context/destination";
 import Destination from "./title/Destination";
 import Coords from "./title/Coords";
+import { pressedDefault } from "utils/helpers";
 
 type TitleProps = {
   destination: DestinationType;
 };
 
 export default function Title(props: TitleProps) {
+  const router = useRouter();
+
   return (
     <View style={styles.container}>
-      <Image source={require("assets/img/logo-icon.svg")} style={styles.logo} />
+      <Pressable
+        onPress={() => router.push({ pathname: "/search", params: { destinationsOnly: true } })}
+        style={({ pressed }) => pressedDefault(pressed)}
+        hitSlop={12}
+      >
+        <Image source={require("assets/img/logo-icon.svg")} style={styles.logo} />
+      </Pressable>
 
       {props.destination.name && (
         <View style={styles.text}>
