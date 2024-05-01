9be6910ce9e3872c83cae74ea6092a47763cee83
Adam Johnson <adam@adamjohnson.org>
2024-05-01 14:04:41 +1000

    Update Controls.tsx

--- components/home/map/Controls.tsx
+++ components/home/map/Controls.tsx
@@ -36,12 +36,9 @@ export default function Controls(props: ControlsProps) {
     } else if (status === "granted") {
       // Device location access granted
       const location = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.Low });
+      const destination = detectDestination(location.coords.latitude, location.coords.longitude);
 
-      if (
-        props.destination.region &&
-        location.coords.latitude === props.destination.region.latitude &&
-        location.coords.longitude === props.destination.region.longitude
-      ) {
+      if (props.destination.id === destination.id) {
         // Device location hasn't changed
         props.setIsLoading(false);
 
@@ -55,7 +52,6 @@ export default function Controls(props: ControlsProps) {
       } else {
         // Device location is different
         setGems([]); // Clear Playbook Gems
-        const destination = detectDestination(location.coords.latitude, location.coords.longitude);
         props.setDestination(destination);
       }
     } else {
